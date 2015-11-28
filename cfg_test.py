a427303671c54f4c51a2a7b4d0a71a597a19af58
U-MAIN\dmc3413 <dmc3413@ce-alien-01.main.ad.rit.edu>
2015-11-27 19:20:16 -0500

    ngram set maker added to corpus_parsing

---
+++ cfg_test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 18:11:22 2015
+
+@author: dmc3413
+"""
+
+import nltk
+
+groucho_grammar = nltk.CFG.fromstring("""
+    S -> NP VP
+    PP -> P NP
+    NP -> Det N | Det N PP | 'I'
+    VP -> V NP | VP PP
+    Det -> 'an' | 'my'
+    N -> 'elephant' | 'pajamas'
+    V -> 'shot'
+    P -> 'in'
+    """);
+    
+print(groucho_grammar)
+
+
+sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
+
+
+parser = nltk.ChartParser(groucho_grammar)
+
+for tree in parser.parse(sent):
+    print(tree)
--- corpora/corpora_parsing.py
+++ corpora/corpora_parsing.py
@@ -3,6 +3,8 @@ Program to do operations on the corpora
 '''
 import nltk
 from nltk import word_tokenize
+from nltk.util import ngrams
+import re
 import os
 import string
 
@@ -33,6 +35,63 @@ def get_unique_tags(path):
         print(pos + ": " + str(len(pos_tags[pos])))
 
     #print(pos_tags['NN'])
+	
+def get_ngrams(path,n):
+    '''
+    Gets a set of ngrams from a path
+    '''
+    data = ""
 
+    # Get all text from selected path
+    full_path = os.getcwd() + path
+    for i in os.listdir(full_path):
+        if i.endswith(".txt"):
+            with open(full_path + '/' + i, 'r') as f:
+                data += f.read().replace('\n', ' ')
+
+
+    # Separt lists for sentances
+    sentences = [[]]
+    addedWord = []
+    idx = 0;
+    for word in data.split():
+        if "." not in word:     
+            word = re.sub("[,-\/#'!?\"$%|<>\^&\*\]\[;:\+{}=\-_`~()]","",word)
+            sentences[idx].append(word)
+            addedWord.append(word);
+        elif re.search('[0-9].[0-9]',word) != None:
+            sentences[idx].append(word)
+            addedWord.append(word);
+        else:
+            word = re.sub("[,-\/#'!?\"$%|<>\^&\*\]\[;:\+{}=\-_`~()]","",word)
+            word = word.replace('.','');
+            sentences[idx].append(word)
+            addedWord.append(word);
+            sentences.append([]);
+            idx = idx + 1;
+            
+    grams = [];
+    for sent in sentences:
+        tup = ngrams(sent, n);
+        for g in tup:
+            grams.append(g)
+
+
+    return set(grams);
+    
+    
+    
+#    ngrams(data, n)
+		
+
+    #print(pos_tags['NN'])
+	
+	
 if __name__ == '__main__':
-    get_unique_tags('/computer_science')
+#    get_unique_tags('/nanocomputing')
+    grams = get_ngrams('/nanocomputing',4)
+        
+     
+#    for g in grams:
+#        print(g)
+#        print( "\n")
