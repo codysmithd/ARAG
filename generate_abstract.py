d34b238e47c4c4c014db27696584c39b72e3aba6
U-MAIN\dmc3413 <dmc3413@ce-alien-01.main.ad.rit.edu>
2015-12-04 16:28:53 -0500

    working on readability metric

--- generate_abstract.py
+++ generate_abstract.py
@@ -3,26 +3,36 @@ This program generates an abstract for a report given a training corpus
 Usage: python generate_abstract.py <path_to_corpus>
 """
 import argparse
+import nltk
 from nltk.parse.generate import generate
 from corpora.corpora_parsing import processCorpus
 from nltk import word_tokenize
+#from curses.ascii import isdigit 
+from nltk.corpus import cmudict 
 
-def scoreSentence(s):
+def nsyl(word,dic): 
+   return [len(list(y for y in x if  y[-1].isdigit() ) ) for x in dic[word.lower()]] 
+
+def scoreSentence(s,d):
     '''
     Scores a given genrated sentence based on certain criteria
     '''
 
     score = 0
+    
+    syllables = 0
+    for word in s:
+        syllables += len(nsyl(word,d))
 
     # Duplicate words? Subtract from it's score
     score += len(set(s)) - len(s)
 
-    return score
+    return score,syllables
 
 def main():
 
     # Config
-    MAX_NUM_SENTENCES = 1000
+    MAX_NUM_SENTENCES = 100
 
     # Argument Parsing
     parser = argparse.ArgumentParser(description="""
@@ -39,13 +49,19 @@ def main():
     # Print out max sentences generated from the cfg
     best_score = float('-inf')
     best_s = ''
+    
+    d = cmudict.dict() 
+
     for s in generate(cfg_grammer, depth=3, n=MAX_NUM_SENTENCES):
-        s_score = scoreSentence(s)
+        s_score, syllables = scoreSentence(s,d)
         if s_score > best_score and len(s) > 0:
             best_score = s_score
             best_s = s
+            
+    
     print(best_s)
     print(best_score)
-
+    print(syllables)
+    
 if __name__ == '__main__':
     main()
